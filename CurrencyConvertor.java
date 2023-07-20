1ae6617d2566fa027ac1dfd33859f2c4367f6808
Juan Obando <juanobando.developer@gmail.com>
2023-07-20 16:52:09 -0500

    refactor: change CURRENCIES array by CURRENCIES HashMap, add destinyCurrency attribute, fill hashmap on constructor, change function name from currency to setDestinyCurrency, add generateCurrencyOptions and separateSelection functions, remove switch sentence of showResult function and add javadoc comments

--- CurrencyConvertor.java
+++ CurrencyConvertor.java
@@ -1,24 +1,42 @@
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.swing.JOptionPane;
 
+/**
+ * this class is the child of Convertor and converts currencies
+ * 
+ * @version 1.0.0
+ * @since 1.0.0
+ * @see Convertor
+ * 
+ * @autor JuanObandoDev
+ */
 public class CurrencyConvertor extends Convertor {
-    private static final String[] CURRENCIES = {
-            "Dollar",
-            "Euro",
-            "Pound",
-            "Yen"
-    };
+    private static final Map<String, Double> CURRENCIES = new HashMap<String, Double>();
     private String currency;
+    private String destinyCurrency;
 
+    /**
+     * This constructor calls the method currency() and sets the currency
+     */
     public CurrencyConvertor() {
         super();
-        this.currency = currency();
+        CURRENCIES.put("Dollars", 0.00024);
+        CURRENCIES.put("Euros", 0.00022);
+        CURRENCIES.put("Pounds", 0.00019);
+        CURRENCIES.put("Yens", 0.03473);
+        this.destinyCurrency = setDestinyCurrency();
     }
 
-    public String currency() {
-        String[] options = new String[CURRENCIES.length];
-        for (int i = 0; i < CURRENCIES.length; i++) {
-            options[i] = "Pesos to " + CURRENCIES[i];
-        }
+    /**
+     * This method returns the currency to convert
+     * 
+     * @return separateSelection(selected)
+     * @see #separateSelection(String selected)
+     */
+    public String setDestinyCurrency() {
+        String[] options = generateCurrencyOptions();
         String selected = (String) JOptionPane.showInputDialog(
                 null,
                 "Select a currency",
@@ -27,27 +45,57 @@ public class CurrencyConvertor extends Convertor {
                 null,
                 options,
                 "None");
-        return selected.replace("Pesos to ", "");
+        return separateSelection(selected);
     }
 
+    /**
+     * This method separates the currency from the selected option and returns it
+     * 
+     * selected is a string like "Pesos to Dollar" so we need to separate the
+     * currency
+     * getting an array like ["Pesos", "to", "Dollar"]
+     * 
+     * @param selected
+     * @return parts[2];
+     */
+    public String separateSelection(String selected) {
+        String[] parts = selected.split(" ");
+        this.currency = parts[0];
+        return parts[2];
+    }
+
+    /**
+     * This method generates the options for the currency convertor
+     * 
+     * @return options
+     */
+    public String[] generateCurrencyOptions() {
+        String[] options = new String[CURRENCIES.size() * 2];
+        int j = 0;
+        for (int i = 0; i < options.length; i++) {
+            if (i < 4) {
+                options[i] = "Pesos to " + CURRENCIES.keySet().toArray()[i];
+            } else {
+                options[i] = CURRENCIES.keySet().toArray()[j] + " to Pesos";
+                j++;
+            }
+        }
+        return options;
+    }
+
+    /**
+     * 
+     */
     public void showResult() {
         double result = 0;
-        switch (this.currency) {
-            case "Dollar":
-                result = super.getValueToConvert() * 0.00024;
-                break;
-            case "Euro":
-                result = super.getValueToConvert() * 0.00022;
-                break;
-            case "Pound":
-                result = super.getValueToConvert() * 0.00019;
-                break;
-            case "Yen":
-                result = super.getValueToConvert() * 0.03473;
-                break;
+        if (this.currency.equals("Pesos")) {
+            result = super.getValueToConvert() * CURRENCIES.get(this.destinyCurrency);
+        } else {
+            result = super.getValueToConvert() / CURRENCIES.get(this.currency);
         }
         JOptionPane.showMessageDialog(
                 null,
-                "$" + super.getValueToConvert() + " pesos is $" + String.format("%.2f", result) + " " + this.currency);
+                "$" + super.getValueToConvert() + " " + this.currency + " is $" + String.format("%.2f", result) + " "
+                        + this.destinyCurrency);
     }
 }
